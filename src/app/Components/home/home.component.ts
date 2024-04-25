03cc95347970a60c595201f13b6befe8b851bad4
unknown <jasimaboobacker1>
2024-04-25 19:44:55 +0530

    message

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent,FormsModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  public loginshow=true;
+
+  constructor(private router:Router){}
+
+  ngOnInit(): void {
+    this.LoginCheck();
+  }
+
+
+  LoginNavigate(){
+    this.router.navigateByUrl('login')
+  }
+
+  LoginCheck(){
+    if(sessionStorage.getItem('token')){
+      this.loginshow=false;
+    }
+  }
 
 }
